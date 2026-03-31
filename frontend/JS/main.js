0317a84ae60d40fd5ac8eeec107fa380b7d8c5af
BThibz <buteauthibault@gmail.com>
2026-03-31 19:35:54 +0200

    refactor: consolidate API and WebSocket logic into main.js, removing websocket.js

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -1,3 +1,27 @@
+const API_URL = "http://localhost:8000"; // ou "http://api:8000" en Docker
+
+function startTraining() {
+    return fetch(`${API_URL}/train`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ dataset: "fashion_mnist" })
+    });
+}
+
+const WS_URL = "ws://localhost:8000/ws"; // ou ws://api:8000/ws
+
+let socket = new WebSocket(WS_URL);
+
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    if (data.training_time) {
+        appendResult(data);
+    } else {
+        appendMetric(data);
+    }
+};
+
 document.getElementById("startBtn")
     .addEventListener("click", startTraining);
 
